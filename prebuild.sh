f85ab60f3417cb61498fe1784fc924e6bb6612ea
CoreySsj <104245267+CoreyTayV@users.noreply.github.com>
2025-10-16 06:40:08 -0500

    ci cd 3

--- prebuild.sh
+++ prebuild.sh
@@ -1,33 +1,37 @@
-#!/bin/bash
-set -e
-# Variables de entorno
-# EKS_CLUSTER_NAME="mycluster"
-# ECR_BACKEND_REPO="usermgmt-marco"
-# ECR_FRONTEND_REPO="usermgmt-marco-frontend"
-# IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
-# BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
-# FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
-export EKS_CLUSTER_NAME="mycluster"
-export ECR_BACKEND_REPO="usermgmt-marco"
-export ECR_FRONTEND_REPO="usermgmt-marco-frontend"
-export IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
-export BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
-export FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
+# #!/bin/bash
+# set -e
+# # Variables de entorno
+# # EKS_CLUSTER_NAME="mycluster"
+# # ECR_BACKEND_REPO="usermgmt-marco"
+# # ECR_FRONTEND_REPO="usermgmt-marco-frontend"
+# # IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
+# # BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
+# # FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
+# export EKS_CLUSTER_NAME="mycluster"
+# export ECR_BACKEND_REPO="usermgmt-marco"
+# export ECR_FRONTEND_REPO="usermgmt-marco-frontend"
+# export IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
+# export BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
+# export FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
 
-# DEBUG: verificar que las variables estén definidas
-echo "DEBUG: AWS_ACCOUNT_ID_MARCO = '$AWS_ACCOUNT_ID_MARCO'"
-echo "DEBUG: AWS_REGION_MARCO = '$AWS_REGION_MARCO'"
-echo "DEBUG: BACKEND_URI = '$BACKEND_URI'"
-echo "DEBUG: FRONTEND_URI = '$FRONTEND_URI'"
-echo "DEBUG: IMAGE_TAG = '$IMAGE_TAG'"
-
-
-echo "DEBUG: comando docker build -t $BACKEND_URI:$IMAGE_TAG ./backend"
-docker build -t "$BACKEND_URI:$IMAGE_TAG" ./backend
+# # DEBUG: verificar que las variables estén definidas
+# echo "DEBUG: AWS_ACCOUNT_ID_MARCO = '$AWS_ACCOUNT_ID_MARCO'"
+# echo "DEBUG: AWS_REGION_MARCO = '$AWS_REGION_MARCO'"
+# echo "DEBUG: BACKEND_URI = '$BACKEND_URI'"
+# echo "DEBUG: FRONTEND_URI = '$FRONTEND_URI'"
+# echo "DEBUG: IMAGE_TAG = '$IMAGE_TAG'"
 
 
-echo "Iniciando sesión en Amazon ECR..."
-aws ecr get-login-password --region $AWS_REGION_MARCO | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com
+# echo "DEBUG: comando docker build -t $BACKEND_URI:$IMAGE_TAG ./backend"
+# docker build -t "$BACKEND_URI:$IMAGE_TAG" ./backend
 
-echo "Configurando credenciales para el clúster de EKS $EKS_CLUSTER_NAME..."
-aws eks update-kubeconfig --region $AWS_REGION_MARCO --name $EKS_CLUSTER_NAME
+
+# echo "Iniciando sesión en Amazon ECR..."
+# aws ecr get-login-password --region $AWS_REGION_MARCO | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com
+
+# echo "Configurando credenciales para el clúster de EKS $EKS_CLUSTER_NAME..."
+# aws eks update-kubeconfig --region $AWS_REGION_MARCO --name $EKS_CLUSTER_NAME
+
+pre_build:
+  commands:
+    - echo "Ejecutando pre-build..."  # opcional, solo para log
