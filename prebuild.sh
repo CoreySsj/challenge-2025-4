b155d40a0a646c5dd09584b8e5ac85a3d4a2d09e
CoreySsj <104245267+CoreyTayV@users.noreply.github.com>
2025-10-16 05:32:33 -0500

    ci cd 3

--- prebuild.sh
+++ prebuild.sh
@@ -9,8 +9,8 @@ export K8S_FRONTEND_DEPLOYMENT="frontend"
 export K8S_BACKEND_CONTAINER_NAME="usermgmt"
 export K8S_FRONTEND_CONTAINER_NAME="frontend"
 export IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
-export BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_BACKEND_REPO
-export FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_FRONTEND_REPO
+export BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
+export FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
 
 echo "Iniciando sesión en Amazon ECR..."
 aws ecr get-login-password --region $AWS_REGION_MARCO | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com
