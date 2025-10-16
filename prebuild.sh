f4a41ae7eb5cf76c91db469df45b4dc1a24b444c
CoreySsj <104245267+CoreyTayV@users.noreply.github.com>
2025-10-16 06:13:17 -0500

    ci cd 3

--- prebuild.sh
+++ prebuild.sh
@@ -14,6 +14,11 @@ export IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
 export BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
 export FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
 
+# DEBUG: verificar que las variables estén definidas
+echo "DEBUG: AWS_ACCOUNT_ID_MARCO = '$AWS_ACCOUNT_ID_MARCO'"
+echo "DEBUG: AWS_REGION_MARCO = '$AWS_REGION_MARCO'"
+echo "DEBUG: BACKEND_URI = '$BACKEND_URI'"
+echo "DEBUG: FRONTEND_URI = '$FRONTEND_URI'"
 
 echo "Iniciando sesión en Amazon ECR..."
 aws ecr get-login-password --region $AWS_REGION_MARCO | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com
