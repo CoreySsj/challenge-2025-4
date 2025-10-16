ab110b21c8b448e2d2f66467e0a8266b658c04bb
CoreySsj <104245267+CoreyTayV@users.noreply.github.com>
2025-10-16 06:09:47 -0500

    ci cd 3

--- prebuild.sh
+++ prebuild.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # Variables de entorno
-EKS_CLUSTER_NAME="mycluster"
-ECR_BACKEND_REPO="usermgmt-marco"
-ECR_FRONTEND_REPO="usermgmt-marco-frontend"
-IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
-BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
-FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
+# EKS_CLUSTER_NAME="mycluster"
+# ECR_BACKEND_REPO="usermgmt-marco"
+# ECR_FRONTEND_REPO="usermgmt-marco-frontend"
+# IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
+# BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
+# FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
+export EKS_CLUSTER_NAME="mycluster"
+export ECR_BACKEND_REPO="usermgmt-marco"
+export ECR_FRONTEND_REPO="usermgmt-marco-frontend"
+export IMAGE_TAG=$(echo $CODEBUILD_BUILD_ID | cut -d: -f2 | cut -c 1-7)
+export BACKEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_BACKEND_REPO
+export FRONTEND_URI=$AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com/$ECR_FRONTEND_REPO
+
 
 echo "Iniciando sesión en Amazon ECR..."
 aws ecr get-login-password --region $AWS_REGION_MARCO | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID_MARCO.dkr.ecr.$AWS_REGION_MARCO.amazonaws.com
